c1cdafcd84ac2112d5e2de3de61a787b9bd08087
Novoplast <e.ryser@novoplast.com>
2022-12-01 14:32:02 +0100

    Zefix query hinzugefügt

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from utils import zefix
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'data':{'name':'Elia'}}
+
+@app.get('/zefix')
+def getZefix(uid: str):
+    return zefix.search(uid)
